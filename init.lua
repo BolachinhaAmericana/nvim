d45ef17c9d28aaaeed3a042decf732891ecc6866
Valente <duarte.tz.valente@gmail.com>
2025-03-12 15:14:32 +0000

    fix diff issue on windows

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- solving issue with difff & diff.
+-- vim.g.undotree_DiffCommand = os.getenv("DIFFF") or "diff"
+
+vim.g.undotree_DiffCommand = "~\\Desktop\\Apps\\diffutils\\bin\\difff.exe"
